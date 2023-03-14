134af0c133563316273edd0120c5c59fee7fdba2
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-03-14 11:40:48 -0500

    added deleted link

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -52,5 +52,10 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
     }
 }
