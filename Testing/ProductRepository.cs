952f33537a770cd66f5a475a256e1889f9ff7cb7
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-03-13 23:03:36 -0500

    step 1

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return _conn.Query<Products>("Select * From Products");
+        }
+    }
+}
